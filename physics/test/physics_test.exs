a7b2409bbf0283408cbb9fa76920799ef18ff42a
MatthewFitchett <matt@mattfitchett.com>
2016-01-30 19:24:43 +0000

    Added first physics project

---
+++ physics/test/physics_test.exs
@@ -0,0 +1,12 @@
+defmodule PhysicsTest do
+  use ExUnit.Case
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+
+  test "lol" do
+    teststring = "test"
+    assert "test" == teststring
+  end
+end
